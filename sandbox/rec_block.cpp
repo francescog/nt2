67900ed16052b2fb859a0da35afc46b88e9d81fb
Joel Falcou <joel.falcou@lri.fr>
2011-04-22 08:28:47 -0500

    Updated test for table

--- sandbox/rec_block.cpp
+++ sandbox/rec_block.cpp
@@ -1,12 +1,7 @@
 #include <iostream>
-#include <nt2/sdk/details/type_id.hpp>
-
-#include <boost/fusion/include/make_vector.hpp>
-#include <boost/fusion/include/vector_tie.hpp>
-#include <nt2/sdk/memory/lead_padding.hpp>
-
-#include <nt2/core/container/details/block.hpp>
 #include <nt2/core/container/table.hpp>
+#include <nt2/core/functions/of_size.hpp>
+#include <nt2/core/functions/size.hpp>
 
 using namespace std;
 
@@ -18,123 +13,15 @@ using nt2::container::block;
 
 int main()
 {
-  {
-    typedef block < float
-                  , boost::mpl::int_<1>
-                  , boost::mpl::vector_c<int,9>
-                  , boost::array<int,1>
-                  , boost::mpl::vector_c<int,0>
-                  , nt2::heap_(nt2::memory::allocator<float>)
-                  , nt2::memory::lead_padding
-                  > t_t;
+  nt2::table<foo, nt2::C_index_> xd( nt2::of_size(2,2) );
 
-    boost::array<int,1> sz= {{5}};
-    boost::mpl::vector_c<int,9> bz;
-    t_t  t(bz,sz);
+  std::cout << xd.empty() << "\n";
+  std::cout << xd.size() << "\n";
+  std::cout << xd.size<1>() << " " << xd.lower<1>() << " " << xd.upper<1>() << "\n";
+  std::cout << xd.size<2>() << " " << xd.lower<2>() << " " << xd.upper<2>() << "\n";
+  std::cout << xd.size<3>() << " " << xd.lower<3>() << " " << xd.upper<3>() << "\n";
+  std::cout << xd.size<4>() << " " << xd.lower<4>() << " " << xd.upper<4>() << "\n";
 
-    cout << t( boost::fusion::make_vector(t.lower<1>()) ) << "\n";
-    t( boost::fusion::make_vector(t.lower<1>()) ) = 9.999f;
-    cout << t( boost::fusion::make_vector(t.lower<1>()) ) << "\n";
-
-    cout << t.size() << "\n";
-    cout  << t.lower<1>()
-          << " " << t.upper<1>()
-          << " " << t.size<1>()
-          << "\n";
-  }
-
-  {
-    typedef block < float
-                  , boost::mpl::int_<2>
-                  , boost::mpl::vector_c<int,3,1>
-                  , boost::array<int,2>
-                  , boost::mpl::vector_c<int,0,1>
-                  , nt2::heap_(nt2::memory::allocator<float>)
-                  , nt2::memory::lead_padding
-                  > t_t;
-    boost::array<int,2> sz= {{5,3}};
-    boost::mpl::vector_c<int,3,1> bz;
-    t_t  t(bz,sz);
-
-    cout << t( boost::fusion::make_vector(t.lower<1>(),t.lower<2>()) ) << "\n";
-    t( boost::fusion::make_vector(t.lower<1>(),t.lower<2>()) ) = 9.999f;
-    cout << t( boost::fusion::make_vector(t.lower<1>(),t.lower<2>()) ) << "\n";
-
-    cout << t.size() << "\n";
-    cout  << t.lower<1>()
-          << " " << t.upper<1>()
-          << " " << t.size<1>()
-          << "\n";
-    cout  << t.lower<2>()
-          << " " << t.upper<2>()
-          << " " << t.size<2>()
-          << "\n";
-  }
-  {
-    typedef block < float
-                  , boost::mpl::int_<2>
-                  , boost::mpl::vector_c<int,3,1>
-                  , boost::array<int,2>
-                  , boost::mpl::vector_c<int,1,0>
-                  , nt2::heap_(nt2::memory::allocator<float>)
-                  , nt2::memory::lead_padding
-                  > t_t;
-    boost::array<int,2> sz= {{5,3}};
-    boost::mpl::vector_c<int,3,1> bz;
-    t_t  t(bz,sz);
-
-    cout << t( boost::fusion::make_vector(t.lower<1>(),t.lower<2>()) ) << "\n";
-    t( boost::fusion::make_vector(t.lower<1>(),t.lower<2>()) ) = 9.999f;
-    cout << t( boost::fusion::make_vector(t.lower<1>(),t.lower<2>()) ) << "\n";
-
-    cout << t.size() << "\n";
-    cout  << t.lower<1>()
-          << " " << t.upper<1>()
-          << " " << t.size<1>()
-          << "\n";
-    cout  << t.lower<2>()
-          << " " << t.upper<2>()
-          << " " << t.size<2>()
-          << "\n";
-  }
-
-  {
-    typedef block < foo
-                  , boost::mpl::int_<1>
-                  , boost::mpl::vector_c<int,0>
-                  , boost::array<int,1>
-                  , boost::mpl::vector_c<int,0>
-                  , nt2::heap_(nt2::memory::allocator<foo>)
-                  , nt2::memory::lead_padding
-                  > t_t;
-    boost::array<int,1> sz= {{5}};
-    boost::mpl::vector_c<int,0> bz;
-    t_t  t(bz,sz);
-
-    cout << t.size() << "\n";
-    cout  << t.lower<1>()
-          << " " << t.upper<1>()
-          << " " << t.size<1>()
-          << "\n";
-  }
-
-  {
-    typedef block < foo
-                   , boost::mpl::int_<2>
-                   , boost::mpl::vector_c<int,0,0>
-                   , boost::array<int,2>
-                   , boost::mpl::vector_c<int,0,1>
-                   , nt2::heap_(nt2::memory::allocator<foo>)
-                   , nt2::memory::lead_padding
-                   > t_t;
-     boost::array<int,2> sz= {{5,7}};
-     boost::mpl::vector_c<int,0,0> bz;
-     t_t  t(bz,sz);
-
-     cout << t.size() << "\n";
-     cout  << t.lower<1>()
-           << " " << t.upper<1>()
-           << " " << t.size<1>()
-           << "\n";
-  }
+  std::cout << nt2::size(8.f)(1) << "\n";
+  std::cout << nt2::size(8.f)(2) << "\n";
 }
