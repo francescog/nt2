18829936c404d82d2d777593629ce71dbb3f277a
Joel Falcou <joel.falcou@lri.fr>
2011-04-24 22:00:35 -0500

    Added NT2_PP_IOTA and NT2_PP_REVERSE_IOTA. To do so, NT2_PP_INDEX has been changed and its examples have been updated

--- modules/sdk/examples/details/pp_index.cpp
+++ modules/sdk/examples/details/pp_index.cpp
@@ -5,5 +5,5 @@
 int main()
 {
   // Prints (0 , 1 , 2)
-  std::cout << NT2_PP_DEBUG( BOOST_PP_ENUM(3,NT2_PP_INDEX,~)) << std::endl;
+  std::cout << NT2_PP_DEBUG( BOOST_PP_ENUM(3,NT2_PP_INDEX,0)) << std::endl;
 }
