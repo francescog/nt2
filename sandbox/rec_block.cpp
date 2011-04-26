b68ebec0527c550991efc813ddcc0eda25f183d5
Joel Falcou <joel.falcou@lri.fr>
2011-04-25 19:47:35 -0500

    Reworked table sample

--- sandbox/rec_block.cpp
+++ sandbox/rec_block.cpp
@@ -1,27 +1,59 @@
 #include <iostream>
 #include <nt2/core/container/table.hpp>
 #include <nt2/core/functions/of_size.hpp>
-#include <nt2/core/functions/size.hpp>
+//#include <nt2/core/functions/size.hpp>
 
 using namespace std;
 
-struct foo {float x; double y; };
+//struct foo {float x; double y; };
+//NT2_ADAPT_COMPOSITE(foo, (float, x)(double, y));
 
-NT2_ADAPT_COMPOSITE(foo, (float, x)(double, y));
-
-using nt2::container::block;
+using nt2::container::table;
 
 int main()
 {
-  nt2::table<foo, nt2::C_index_> xd( nt2::of_size(2,2) );
+  {
+    table<float> b1;
 
-  std::cout << xd.empty() << "\n";
-  std::cout << xd.size() << "\n";
-  std::cout << xd.size<1>() << " " << xd.lower<1>() << " " << xd.upper<1>() << "\n";
-  std::cout << xd.size<2>() << " " << xd.lower<2>() << " " << xd.upper<2>() << "\n";
-  std::cout << xd.size<3>() << " " << xd.lower<3>() << " " << xd.upper<3>() << "\n";
-  std::cout << xd.size<4>() << " " << xd.lower<4>() << " " << xd.upper<4>() << "\n";
+    using std::cout;
 
-  std::cout << nt2::size(8.f)(1) << "\n";
-  std::cout << nt2::size(8.f)(2) << "\n";
+    cout << "4 : " << b1.nDims() << "\n";
+    cout << b1.empty() << "\n";
+    cout << b1.size() << " " << b1.size(0) << "\n";
+    cout << b1.lower(1) << " " << b1.upper(1) << " " << b1.size(1) << "\n";
+    cout << b1.lower(2) << " " << b1.upper(2) << " " << b1.size(2) << "\n";
+    cout << b1.lower(3) << " " << b1.upper(3) << " " << b1.size(3) << "\n";
+    cout << b1.lower(4) << " " << b1.upper(4) << " " << b1.size(4) << "\n";
+    cout << "\n";
+  }
+
+  {
+    table<float > b1( nt2::of_size(3,4) );
+
+    using std::cout;
+
+    cout << "2 : " << b1.nDims() << "\n";
+    cout << b1.empty() << "\n";
+    cout << b1.size() << " " << b1.size(0) << "\n";
+    cout << b1.lower(1) << " " << b1.upper(1) << " " << b1.size(1) << "\n";
+    cout << b1.lower(2) << " " << b1.upper(2) << " " << b1.size(2) << "\n";
+    cout << b1.lower(3) << " " << b1.upper(3) << " " << b1.size(3) << "\n";
+    cout << b1.lower(4) << " " << b1.upper(4) << " " << b1.size(4) << "\n";
+    cout << "\n";
+  }
+
+  {
+    table<float, nt2::of_size_<3,1,4> > b1;
+
+    using std::cout;
+
+    cout << "3 : " << b1.nDims() << "\n";
+    cout << b1.empty() << "\n";
+    cout << b1.size() << " " << b1.size(0) << "\n";
+    cout << b1.lower(1) << " " << b1.upper(1) << " " << b1.size(1) << "\n";
+    cout << b1.lower(2) << " " << b1.upper(2) << " " << b1.size(2) << "\n";
+    cout << b1.lower(3) << " " << b1.upper(3) << " " << b1.size(3) << "\n";
+    cout << b1.lower(4) << " " << b1.upper(4) << " " << b1.size(4) << "\n";
+    cout << "\n";
+  }
 }
