a0e742dc042fd685766a3c27085bc2230948930b
Joel Falcou <joel.falcou@lri.fr>
2011-05-02 15:03:21 -0500

    small update for size test

--- sandbox/rec_block.cpp
+++ sandbox/rec_block.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <nt2/core/container/table.hpp>
 #include <nt2/core/functions/of_size.hpp>
-//#include <nt2/core/functions/size.hpp>
+#include <nt2/core/functions/size.hpp>
 
 using namespace std;
 
@@ -10,50 +10,48 @@ using namespace std;
 
 using nt2::container::table;
 
+/////////////////////////////////////////////////////////////////////////////
+// Implementation when type A0 is an expression
+/////////////////////////////////////////////////////////////////////////////
+NT2_REGISTER_DISPATCH ( tag::size_, tag::cpu_
+                      , (A0)(S)
+                      , ((block_< fundamental_<A0>,S>))
+                      )
+
+namespace nt2 { namespace ext
+{
+  template<class S, class Dummy>
+  struct  call< tag::size_(tag::block_<tag::fundamental_,S>)
+              , tag::cpu_, Dummy
+              >
+        : callable
+  {
+    template<class Sig> struct result;
+
+    template<class This,class A0>
+    struct result<This(A0)>
+    {
+      typedef typename meta::strip<A0>::type::sizes_type const& type;
+    };
+
+    NT2_FUNCTOR_CALL(1) { return a0.sizes(); }
+  };
+} }
+
 int main()
 {
   {
-    table<float> b1;
+    table<float, nt2::_3D> b1( nt2::of_size(3,4,1) );
+    table<float, nt2::_2D> b2( nt2::of_size(3,4) );
+        using std::cout;
 
-    using std::cout;
-
-    cout << "4 : " << b1.nDims() << "\n";
-    cout << b1.empty() << "\n";
-    cout << b1.size() << " " << b1.size(0) << "\n";
-    cout << b1.lower(1) << " " << b1.upper(1) << " " << b1.size(1) << "\n";
-    cout << b1.lower(2) << " " << b1.upper(2) << " " << b1.size(2) << "\n";
-    cout << b1.lower(3) << " " << b1.upper(3) << " " << b1.size(3) << "\n";
-    cout << b1.lower(4) << " " << b1.upper(4) << " " << b1.size(4) << "\n";
-    cout << "\n";
+    cout << nt2::size(3.53f)     << "\n";
+    cout << nt2::size(b1)     << "\n";
+    cout << nt2::size(b1)     << "\n";
+    cout << nt2::size(b1+b1)  << "\n";
+    cout << nt2::size(b1+b2)  << "\n";
+    cout << nt2::size(b2+b1)  << "\n";
+    cout << nt2::size(b2+b2)  << "\n";
   }
 
-  {
-    table<float > b1( nt2::of_size(3,4) );
-
-    using std::cout;
-
-    cout << "2 : " << b1.nDims() << "\n";
-    cout << b1.empty() << "\n";
-    cout << b1.size() << " " << b1.size(0) << "\n";
-    cout << b1.lower(1) << " " << b1.upper(1) << " " << b1.size(1) << "\n";
-    cout << b1.lower(2) << " " << b1.upper(2) << " " << b1.size(2) << "\n";
-    cout << b1.lower(3) << " " << b1.upper(3) << " " << b1.size(3) << "\n";
-    cout << b1.lower(4) << " " << b1.upper(4) << " " << b1.size(4) << "\n";
-    cout << "\n";
-  }
-
-  {
-    table<float, nt2::of_size_<3,1,4> > b1;
-
-    using std::cout;
-
-    cout << "3 : " << b1.nDims() << "\n";
-    cout << b1.empty() << "\n";
-    cout << b1.size() << " " << b1.size(0) << "\n";
-    cout << b1.lower(1) << " " << b1.upper(1) << " " << b1.size(1) << "\n";
-    cout << b1.lower(2) << " " << b1.upper(2) << " " << b1.size(2) << "\n";
-    cout << b1.lower(3) << " " << b1.upper(3) << " " << b1.size(3) << "\n";
-    cout << b1.lower(4) << " " << b1.upper(4) << " " << b1.size(4) << "\n";
-    cout << "\n";
-  }
 }
