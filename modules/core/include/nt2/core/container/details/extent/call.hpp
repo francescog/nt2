c7cfbf943b2a05eac972847b1f0c22f7d52ce427
Joel Falcou <joel.falcou@lri.fr>
2011-05-25 00:07:42 -0500

    Split call and evaluate on extent

--- modules/core/include/nt2/core/container/details/extent/call.hpp
+++ modules/core/include/nt2/core/container/details/extent/call.hpp
@@ -9,4 +9,59 @@
 #ifndef NT2_CORE_CONTAINER_DETAILS_EXTENT_CALL_HPP_INCLUDED
 #define NT2_CORE_CONTAINER_DETAILS_EXTENT_CALL_HPP_INCLUDED
 
+#include <nt2/sdk/meta/as.hpp>
+#include <nt2/sdk/dsl/compute.hpp>
+#include <nt2/sdk/dsl/category.hpp>
+
+namespace nt2 { namespace meta
+{
+  template<class Target>
+  struct  compute<tag::extent_,Target>
+        : boost::proto::
+          call< functor < tag::extent_
+                        , Target
+                        > ( boost::proto::_value
+                          , boost::proto::_state
+                          , boost::proto::_data
+                          )
+              >
+  {};
+}}
+
+////////////////////////////////////////////////////////////////////////////////
+// Register terminal handlers for SIMD expression - emulated case
+////////////////////////////////////////////////////////////////////////////////
+NT2_REGISTER_DISPATCH_TPL (  tag::extent_,tag::cpu_
+                          , (class A0)(class A1)(class A2)(std::size_t N)
+                          , ((array_<arithmetic_<A0>,N>))
+                            ((target_<array_<arithmetic_<A1>,N> >))
+                            (integer_<A2>)
+                          )
+
+namespace nt2 { namespace ext
+{
+  template<class Dummy,std::size_t N>
+  struct call<tag::extent_( tag::array_<tag::arithmetic_,N>
+                          , tag::target_< tag::array_<tag::arithmetic_,N> >
+                          , tag::integer_
+                          ),tag::cpu_,Dummy>
+  : callable
+  {
+    template<class Sig> struct result;
+
+    template<class This, class Value, class State, class Data>
+    struct result<This(Value,State,Data)>
+    {
+      typedef typename meta::strip<Value>::type::value_type  type;
+    };
+
+    template<class Value, class State, class Data> inline
+    typename result<call(Value,State,Data)>::type
+    operator()( Value& v, State& , Data& pos) const
+    {
+      return v[pos];
+    }
+  };
+} }
+
 #endif
