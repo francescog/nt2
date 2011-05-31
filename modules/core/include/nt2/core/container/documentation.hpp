8cca99f289725227005a4a614c43d476dd99b161
Joel Falcou <joel.falcou@lri.fr>
2011-05-31 17:18:05 -0500

    Added global core documentation to its proper place

---
+++ modules/core/include/nt2/core/container/documentation.hpp
@@ -0,0 +1,21 @@
+#error "This file is for documentation purpose only."
+
+/*!
+ * \defgroup containers NT2 Containers
+ * Classes and function related to containers usage
+ */
+
+/*!
+ * \ingroup containers
+ * \defgroup containershelper Containers helpers
+ * Classes and function related to containers usage
+ */
+
+//==============================================================================
+/*!
+ * \ingroup containers
+ * \namespace nt2::containers
+ * Namespace holding all NT2 containers and container-related classes
+ */
+//==============================================================================
+namespace nt2 { namespace containers {} }
