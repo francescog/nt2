5de2033fe74e7f21a9e96bddeade838139ac9613
Joel Falcou <joel.falcou@lri.fr>
2011-06-08 22:50:46 -0500

    Made of_size use PP_ITERATE and gave it a doc

--- modules/core/include/nt2/core/functions/of_size.hpp
+++ modules/core/include/nt2/core/functions/of_size.hpp
@@ -1,39 +1,70 @@
-//////////////////////////////////////////////////////////////////////////////
-///   Copyright 2003 and onward LASMEA UMR 6602 CNRS/U.B.P Clermont-Ferrand
-///   Copyright 2009 and onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
-///
-///          Distributed under the Boost Software License, Version 1.0
-///                 See accompanying file LICENSE.txt or copy at
-///                     http://www.boost.org/LICENSE_1_0.txt
-//////////////////////////////////////////////////////////////////////////////
+#if !BOOST_PP_IS_ITERATING
+//==============================================================================
+//         Copyright 2003 & onward LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 & onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
 #ifndef NT2_CORE_FUNCTIONS_OF_SIZE_HPP_INCLUDED
 #define NT2_CORE_FUNCTIONS_OF_SIZE_HPP_INCLUDED
 
+/*!
+ * \file
+ * \brief Defines and implements the of_size functions
+ */
+
 #include <nt2/core/container/extent.hpp>
+#include <boost/preprocessor/iteration/iterate.hpp>
+#include <boost/preprocessor/repetition/enum_params.hpp>
+#include <boost/preprocessor/repetition/enum_binary_params.hpp>
 
 namespace nt2
 {
-  //////////////////////////////////////////////////////////////////////////////
-  // Return an empty extent
-  //////////////////////////////////////////////////////////////////////////////
+  #if defined(DOXYGEN_ONLY)
+  //============================================================================
+  /*!
+   * Builds a nD extent from a list of \c n dimension values.
+   *
+   * \param d Values to use as dimensions
+   *
+   * \usage
+   *
+   * \code
+   * #include <iostream>
+   * #include <nt2/core/functions/of_size.hpp>
+   *
+   * int main()
+   * {
+   *   std::cout << nt2::of_size(3,3,4) << "\n";
+   * }
+   * \endcode
+   */
+  //============================================================================
+  template<class ...Dims> extent<_nD> of_size(Dims const& d...);
+  #endif
+
   extent<_0D> of_size() { return extent<_0D>(); }
 
-  #define M0(z,n,t)                                                             \
-  template<BOOST_PP_ENUM_PARAMS(n,class D)>                                     \
-  extent<BOOST_PP_CAT(BOOST_PP_CAT(_,n),D)>                                     \
-  of_size(BOOST_PP_ENUM_BINARY_PARAMS(n,const D, &d))                           \
-  {                                                                             \
-    extent<BOOST_PP_CAT(BOOST_PP_CAT(_,n),D)> that(BOOST_PP_ENUM_PARAMS(n,d));  \
-    return that;                                                                \
-  }                                                                             \
-  /**/
-
-  //////////////////////////////////////////////////////////////////////////////
-  // Builds of_size overload up to NT2_MAX_DIMENSIONS
-  //////////////////////////////////////////////////////////////////////////////
-  BOOST_PP_REPEAT_FROM_TO(1,BOOST_PP_INC(NT2_MAX_DIMENSIONS),M0,~)
-
-  #undef M0
+  #define   BOOST_PP_FILENAME_1 "nt2/core/functions/of_size.hpp"
+  #define   BOOST_PP_ITERATION_LIMITS (1, NT2_MAX_DIMENSIONS)
+  #include  BOOST_PP_ITERATE()
 }
+
+#endif
+
+#else
+#define N BOOST_PP_ITERATION()
+
+  template<BOOST_PP_ENUM_PARAMS(N,class D)>
+  extent<BOOST_PP_CAT(BOOST_PP_CAT(_,N),D)>
+  of_size(BOOST_PP_ENUM_BINARY_PARAMS(N,const D, &d))
+  {
+    extent<BOOST_PP_CAT(BOOST_PP_CAT(_,N),D)> that(BOOST_PP_ENUM_PARAMS(N,d));
+    return that;
+  }
+
+#undef N
 
 #endif
