66a6b3d5bc24b64ecabfb6a832c4c5c72bba0dd5
Joel Falcou <joel.falcou@lri.fr>
2011-06-14 08:53:09 -0500

    Added specialization of size for table terminals

---
+++ modules/core/include/nt2/core/container/details/table/size.hpp
@@ -0,0 +1,43 @@
+////////////////////////////////////////////////////////////////////////////////
+///   Copyright 2003 and onward LASMEA UMR 6602 CNRS/U.B.P Clermont-Ferrand
+///   Copyright 2009 and onward LRI    UMR 8623 CNRS/Univ Paris Sud XI
+///
+///          Distributed under the Boost Software License, Version 1.0
+///                 See accompanying file LICENSE.txt or copy at
+///                     http://www.boost.org/LICENSE_1_0.txt
+////////////////////////////////////////////////////////////////////////////////
+#ifndef NT2_CORE_CONTAINER_DETAILS_TABLE_SIZE_HPP_INCLUDED
+#define NT2_CORE_CONTAINER_DETAILS_TABLE_SIZE_HPP_INCLUDED
+
+#include <nt2/sdk/meta/fusion.hpp>
+#include <nt2/core/container/details/block/category.hpp>
+
+/////////////////////////////////////////////////////////////////////////////
+// Implementation when type A0 is an extent value
+/////////////////////////////////////////////////////////////////////////////
+NT2_REGISTER_DISPATCH ( tag::size_, tag::cpu_
+                      , (A0)(Storage)
+                      , ((heap_<arithmetic_<A0>,Storage>))
+                      )
+
+namespace nt2 { namespace ext
+{
+  template<class Storage, class Dummy>
+  struct  call< tag::size_(tag::heap_<tag::arithmetic_,Storage>)
+              , tag::cpu_, Dummy
+              >
+        : callable
+  {
+    template<class Sig> struct result;
+
+    template<class This,class A0>
+    struct result<This(A0)>
+    {
+      typedef typename meta::strip<A0>::type::sizes_type const& type;
+    };
+
+    NT2_FUNCTOR_CALL(1) { return a0.sizes(); }
+  };
+} }
+
+#endif
